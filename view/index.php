ba30e055b2c5d48eb45e7d73c0f8ce9143513b55
Michael Berryman <berryman.michael@yahoo.com>
2021-03-05 18:14:01 -0800

    add basic pages

---
+++ view/index.php
@@ -0,0 +1,32 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Anna's Store </title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li>new</li>
+            <li>about</li>
+            <li>contact</li>
+            <li><a href="register.php">Create Account</a></li>
+            <li><a href="login.php">Sign In</a></li>
+        </ul>
+    </nav>
+    <div>
+        <p>welcome!</p>
+    </div>
+    <footer>
+        <ul>
+            <li>contact us</li>
+            <li>social media</li>
+        </ul>
+    </footer>
+</body>
+</html>
